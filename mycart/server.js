8fc1f49130fd522130cf165f4b2bceaeeb4b81f5
chatbotapps <madihan120@gmail.com>
2020-10-27 14:22:25 +0530

    v

--- mycart/server.js
+++ mycart/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/ecom/mycart/src'));
+app.use(express.static(__dirname + '/mycart/src'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/ecom/mycart/src/index.html'));
+  res.sendFile(path.join(__dirname + '/mycart/src/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
